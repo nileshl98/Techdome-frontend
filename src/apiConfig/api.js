102b16ec8e9d4986156a7907faf8840edaf4f9a0
root <nilesh>
2024-11-21 18:08:50 +0000

    k8s files added

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://18.215.244.232:8000/api"; // Updated with backend server IP
+const baseUrl = "http://50.17.13.247:8000/api"; // Updated with backend server IP
 
 const instance = axios.create({
     baseURL: `${baseUrl}`,
